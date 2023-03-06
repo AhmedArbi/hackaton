81f9dc06621f716353567f4eb879860e68645543
Arbi.Ahmed177 <aarabi@gopartner.fr>
2023-03-06 12:14:50 +0100

    adding ec2 module

--- 01_ec2/main.tf
+++ 01_ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "workshop" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = ["${aws_security_group.workshop_sg.id}"]
-  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+  subnet_id              = var.public_subnet
   iam_instance_profile   = aws_iam_role.workshop_role.name
 
   tags = merge(local.common_tags, { "Name" : "workshop" })
--- 01_ec2/variables.tf
+++ 01_ec2/variables.tf
@@ -35,3 +35,7 @@ variable "vpc_id" {
   type = string
 
 }
+
+variable "public_subnet" {
+  type = string
+}
